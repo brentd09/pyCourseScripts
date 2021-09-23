40f2d8a08905f3133861f2d3b568b9699f932116
Brent Denny <brent.denny@ddls.com.au>
2021-09-24 00:14:26 +1000

    new scripts

---
+++ BOMData.py
@@ -0,0 +1,10 @@
+DataBom = '20210922211024'
+TempData = {
+  'Year' : DataBom[0:4],
+  'Month' : DataBom[4:6],
+  'Day' : DataBom[6:8],
+  'Temp' : DataBom[8:10],
+  'Pres' : DataBom[10:14]
+}
+
+print(TempData)
