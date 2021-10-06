bd66d9886e79551ddb8dc56faa571119bce4cd0c
brent denny <brent.denny@ddls.com.au>
2021-10-07 07:50:16 +1000

    changes

--- PythonEssentials/Testing_functions.py
+++ PythonEssentials/Testing_functions.py
@@ -1,1 +1,20 @@
-print(str(bin(0))[2:])
+class Staff:
+  def __init__(self,SPosition,SName,SPay):
+    self.position = SPosition
+    self.name = SName
+    self.pay = SPay
+    print('Creating staff member')
+
+  def __str__(self):
+    return 'Position = {}, Name = {}, Pay = {}'.format(self.position,self.name,self.pay)
+
+  def CalculatePay(self):
+    prompt = '\nEnter the number og hours worked for {} '.format(self.name)
+    hours = input(prompt)
+    prompt = 'Enter the hourly rate for {} '.format(self.name)
+    hourlyrate = input(prompt)
+    self.pay = int(hours) * int(hourlyrate)
+
+OfficeStaff = Staff('Admin','Betty',0)
+print(OfficeStaff.CalculatePay())
+print(OfficeStaff)
